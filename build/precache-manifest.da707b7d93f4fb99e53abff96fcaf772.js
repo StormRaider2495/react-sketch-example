e9c44456714faf8b53fce6c15e88a57e32bcc40e
arunitmazumdar <arunit.mazumdar@learningmate.com>
2020-07-24 16:35:32 +0530

    build files added

---
+++ build/precache-manifest.da707b7d93f4fb99e53abff96fcaf772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f626e9006ef2628704",
+    "url": "/static/css/main.85d7355b.chunk.css"
+  },
+  {
+    "revision": "09f626e9006ef2628704",
+    "url": "/static/js/main.c564b40a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0216712d5e1eb1d96822",
+    "url": "/static/js/2.b6f93897.chunk.js"
+  },
+  {
+    "revision": "e8a2c90c36c51b8cc13674de61c6b507",
+    "url": "/index.html"
+  }
+];
